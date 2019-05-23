5549af209f6fb70a7cb8a44e29f374f466a2c9fd
Huan Wang <fredwanghuan@gmail.com>
2019-05-22 22:31:06 -0600

    add sample override file

---
+++ modoverrides.lua
@@ -0,0 +1,226 @@
+return {
+    -- Geometric Placement
+    ["workshop-351325790"] = { enabled = false,
+        configuration_options = {
+        -- CTRL Turns Mod
+        -- description = On, data = true
+        -- description = Off, data = false
+        ["CTRL"] = true,
+        -- Options Button
+        -- description = A, data = "A"
+        -- description = B, data = "B"
+        -- description = C, data = "C"
+        -- description = D, data = "D"
+        -- description = E, data = "E"
+        -- description = F, data = "F"
+        -- description = G, data = "G"
+        -- description = H, data = "H"
+        -- description = I, data = "I"
+        -- description = J, data = "J"
+        -- description = K, data = "K"
+        -- description = L, data = "L"
+        -- description = M, data = "M"
+        -- description = N, data = "N"
+        -- description = O, data = "O"
+        -- description = P, data = "P"
+        -- description = Q, data = "Q"
+        -- description = R, data = "R"
+        -- description = S, data = "S"
+        -- description = T, data = "T"
+        -- description = U, data = "U"
+        -- description = V, data = "V"
+        -- description = W, data = "W"
+        -- description = X, data = "X"
+        -- description = Y, data = "Y"
+        -- description = Z, data = "Z"
+        ["KEYBOARDTOGGLEKEY"] = "B",
+        -- Toggle Button
+        -- description = A, data = "A"
+        -- description = B, data = "B"
+        -- description = C, data = "C"
+        -- description = D, data = "D"
+        -- description = E, data = "E"
+        -- description = F, data = "F"
+        -- description = G, data = "G"
+        -- description = H, data = "H"
+        -- description = I, data = "I"
+        -- description = J, data = "J"
+        -- description = K, data = "K"
+        -- description = L, data = "L"
+        -- description = M, data = "M"
+        -- description = N, data = "N"
+        -- description = O, data = "O"
+        -- description = P, data = "P"
+        -- description = Q, data = "Q"
+        -- description = R, data = "R"
+        -- description = S, data = "S"
+        -- description = T, data = "T"
+        -- description = U, data = "U"
+        -- description = V, data = "V"
+        -- description = W, data = "W"
+        -- description = X, data = "X"
+        -- description = Y, data = "Y"
+        -- description = Z, data = "Z"
+        ["GEOMETRYTOGGLEKEY"] = "V",
+        -- In-Game Menu
+        -- description = On, data = true
+        -- description = Off, data = false
+        ["SHOWMENU"] = true,
+        -- Show Build Grid
+        -- description = On, data = true
+        -- description = Off, data = false
+        ["BUILDGRID"] = true,
+        -- Grid Geometry
+        -- description = Square, data = "SQUARE"
+        -- description = Diamond, data = "DIAMOND"
+        -- description = X Hexagon, data = "X_HEXAGON"
+        -- description = Z Hexagon, data = "Z_HEXAGON"
+        -- description = Flat Hexagon, data = "FLAT_HEXAGON"
+        -- description = Pointy Hexagon, data = "POINTY_HEXAGON"
+        ["GEOMETRY"] = "SQUARE",
+        -- Refresh Speed
+        -- description = 10%, data = 0.1
+        -- description = 20%, data = 0.2
+        -- description = 30%, data = 0.3
+        -- description = 40%, data = 0.4
+        -- description = 50%, data = 0.5
+        -- description = 60%, data = 0.6
+        -- description = 70%, data = 0.7
+        -- description = 80%, data = 0.8
+        -- description = 90%, data = 0.9
+        -- description = 100%, data = 1
+        -- description = Unlimited, data = false
+        ["TIMEBUDGET"] = 0.1,
+        -- Hide Placer
+        -- description = On, data = true
+        -- description = Off, data = false
+        ["HIDEPLACER"] = false,
+        -- Hide Cursor Item
+        -- description = Hide All, data = 1
+        -- description = Show Number, data = true
+        -- description = Show All, data = false
+        ["HIDECURSOR"] = false,
+        -- Fine Grid Size
+        -- description = 0, data = 0
+        -- description = 2, data = 2
+        -- description = 4, data = 4
+        -- description = 6, data = 6
+        -- description = 8, data = 8
+        -- description = 10, data = 10
+        -- description = 12, data = 12
+        -- description = 14, data = 14
+        -- description = 16, data = 16
+        -- description = 18, data = 18
+        -- description = 20, data = 20
+        ["SMALLGRIDSIZE"] = 10,
+        -- Wall Grid Size
+        -- description = 0, data = 0
+        -- description = 1, data = 1
+        -- description = 2, data = 2
+        -- description = 3, data = 3
+        -- description = 4, data = 4
+        -- description = 5, data = 5
+        -- description = 6, data = 6
+        -- description = 7, data = 7
+        -- description = 8, data = 8
+        -- description = 9, data = 9
+        -- description = 10, data = 10
+        ["MEDGRIDSIZE"] = 6,
+        -- Sandbag Grid Size
+        -- description = 0, data = 0
+        -- description = 1, data = 1
+        -- description = 2, data = 2
+        -- description = 3, data = 3
+        -- description = 4, data = 4
+        -- description = 5, data = 5
+        -- description = 6, data = 6
+        -- description = 7, data = 7
+        -- description = 8, data = 8
+        -- description = 9, data = 9
+        -- description = 10, data = 10
+        ["FLOODGRIDSIZE"] = 5,
+        -- Turf Grid Size
+        -- description = 0, data = 0
+        -- description = 1, data = 1
+        -- description = 2, data = 2
+        -- description = 3, data = 3
+        -- description = 4, data = 4
+        -- description = 5, data = 5
+        ["BIGGRIDSIZE"] = 2,
+        -- Unblocked Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Outlined White, data = "whiteoutline"
+        -- description = Outlined Black, data = "blackoutline"
+        -- description = Hidden, data = "hidden"
+        ["GOODCOLOR"] = "whiteoutline",
+        -- Blocked Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Outlined White, data = "whiteoutline"
+        -- description = Outlined Black, data = "blackoutline"
+        -- description = Hidden, data = "hidden"
+        ["BADCOLOR"] = "blackoutline",
+        -- Nearest Tile Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Outlined White, data = "whiteoutline"
+        -- description = Outlined Black, data = "blackoutline"
+        -- description = Hidden, data = "hidden"
+        ["NEARTILECOLOR"] = "white",
+        -- Unblocked Tile Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Outlined White, data = "whiteoutline"
+        -- description = Outlined Black, data = "blackoutline"
+        -- description = Hidden, data = "hidden"
+        ["GOODTILECOLOR"] = "whiteoutline",
+        -- Blocked Tile Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Outlined White, data = "whiteoutline"
+        -- description = Outlined Black, data = "blackoutline"
+        -- description = Hidden, data = "hidden"
+        ["BADTILECOLOR"] = "blackoutline",
+        -- Unblocked Placer Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Hidden, data = "hidden"
+        ["GOODPLACERCOLOR"] = "white",
+        -- Blocked Placer Color
+        -- description = Green, data = "green"
+        -- description = Blue, data = "blue"
+        -- description = Red, data = "red"
+        -- description = White, data = "white"
+        -- description = Black, data = "black"
+        -- description = Hidden, data = "hidden"
+        ["BADPLACERCOLOR"] = "black",
+        -- Tighter Chests
+        -- description = Yes, data = true
+        -- description = No, data = false
+        ["REDUCECHESTSPACING"] = true,
+        -- Controller Offset
+        -- description = On, data = true
+        -- description = Off, data = false
+        ["CONTROLLEROFFSET"] = false,
+        },
+    },
+}
